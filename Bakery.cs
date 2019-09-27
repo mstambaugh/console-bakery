1193c4fc26c88e1c250c236b59671768043b8e07
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-09-27 15:25:22 -0700

    add methods and arithmetic for pastries, simplify bread methods

--- Bakery.cs
+++ Bakery.cs
@@ -3,14 +3,14 @@ namespace Bakery
   class Bread 
   {
     // auto-implemented properties 
-    public string Loaf { get; set; }
+    // public string Loaf { get; set; }
     public int Quantity { get; set; }
     public int TotalPrice { get; set; } 
 
     // bread constructor
-    public Bread(string loaf, int quantity, int totalprice) 
+    public Bread(int quantity, int totalprice) 
     {
-      Loaf = loaf;
+      // Loaf = loaf;
       Quantity = quantity;
       TotalPrice = totalprice;
     }
@@ -35,17 +35,42 @@ namespace Bakery
       return TotalPrice;
     }
   }   
-}
+
   class Pastries 
   {
     public string Pastry { get; set; }
-    public int Quantity2 { get; set; }
-    public int TotalPrice { get; set; }
-    public Pastries(string pastry, int quantity2, int totalprice)
+    public int PastryQuantity { get; set; }
+    public int PastryPrice { get; set; }
+    public Pastries(string pastry, int PastryQuantity, int PastryPrice)
     {
       Pastry = pastry;
-      Quantity2 = quantity2;
-      TotalPrice = totalprice;
+      PastryQuantity = pastryquantity;
+      PastryPrice = pastryprice;
+    }
+    public int GetPastryQuantity()
+    {
+      return PastryQuantity;
+    }
+    public int GetPastryTotalPrice()
+    {
+      int CheapPastries = 0;
+      // Buy 1 for \$2 or 3 for $5.
+      if (PastryQuantity % 3 == 0)
+      {
+        PastryPrice = (PastryQuantity / 3 ) * 5;
+      }
+      if (PastryQuantity > 3)
+      {
+        // 13/3 = 4 * 5 = 20, 
+        CheapPastries = PastryQuantity / 3;
+        ExpensivePastries = PastryQuantity % 3;
+        PastryPrice = (CheapPastries * 5) + (ExpensivePastries *2);
+      }
+      else
+      {
+        PastryPrice = Quantity * 2;
+      }
+      return TotalPrice;
     }
   }
 }
